4e76ac6495597267f060bdd0a2b46d06aadc235f
Iurii Kogan <koganiurii@gmail.com>
2019-04-09 22:57:34 +0700

    changes to tbody targets classes

--- scripts/main.js
+++ scripts/main.js
@@ -1,7 +1,7 @@
 console.log('this works');
 
-var stock_prices_btn = document.getElementById('load-data-btn');
-var external_data_table = document.getElementById('external-data-table');
+var stock_prices_btn = document.getElementById('stock-prices-btn');
+var stock_prices = document.getElementById('stock-prices');
 stock_prices_btn.onclick = function (t) {
 
 	fetch('https://www.alphavantage.co/query?function=TIME_SERIES_WEEKLY&outputsize=compact&symbol=MSFT&apikey=5MB3SANDM9OTDRY0') // get the data
@@ -9,26 +9,26 @@ stock_prices_btn.onclick = function (t) {
 			return response.json(); // transform the data
 		})
 		.then(function(data) {
-			console.log('stockData_body', stockData_body );
+			console.log('stock-prices', stock_prices);
 
-			var stockData_body = document.createElement('tbody');
+			var stock_prices = document.createElement('tbody');
 
-			stockData.forEach(function(todo) {
+			stock_prices.forEach(function(data) {
 
-				let todo_tr = document.createElement('tr'); // cteates <tr></tr>
+				let data_tr = document.createElement('tr'); // cteates <tr></tr>
 
-				for (value in todo) {
-					let todo_td = document.createElement('td'); // creates <td></td>
-					let text = document.createTextNode(todo[value]); // creates text for the object value
-					todo_td.appendChild(text); // adds the text to the <td>
-					todo_tr.appendChild(todo_td); // adds the <td> to the <tr>
+				for (value in data) {
+					let data_td = document.createElement('td'); // creates <td></td>
+					let text = document.createTextNode(data[value]); // creates text for the object value
+					data_td.appendChild(text); // adds the text to the <td>
+					data_tr.appendChild(data_td); // adds the <td> to the <tr>
 				}
 
-				stockData_body.appendChild(todo_tr); // adds <tr> to <tbody>
+				stockData_body.appendChild(data_tr); // adds <tr> to <tbody>
 
 			});
 
-			external_data_table.appendChild(todos_body); // pushes <tbody> to the DOM inside table
+			stock_prices.appendChild(data_body); // pushes <tbody> to the DOM inside table
 
         });
     };
