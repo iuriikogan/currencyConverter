1128ba7ae0e2831d67b7ad95a4cb742aa2b27501
sjo <sjo@qad.com>
2019-04-09 21:18:54 -0400

    //updates

--- scripts/main.js
+++ scripts/main.js
@@ -1,46 +1,49 @@
-var currenry_prices_btn = function newFunction () {
-	return document.getElementById('.currenry-prices');
-}
+// var currency_prices_btn = function newFunction () {
+// 	return document.getElementById('.currenry-prices');
+// }
 
 var testVar = {
-	eur: 1,
-	usd: 1.3
+    eur: 1,
+    usd: 1.3
 }
 
-
+var currency_prices;
 
 var curData = new XMLHttpRequest();
 curData.open('GET', 'http://data.fixer.io/api/latest?access_key=a754c07aba547f864f841764e00a058c');
 curData.onload = function () {
-	currenry_prices = JSON.parse(curData.responseText);
-	console.log(currenry_prices);
+    currency_prices = JSON.parse(curData.responseText);
+    console.log(currency_prices);
 };
 curData.send();
 
-currenry_prices_btn.onclick = function () {
+function DoSomething() {
+    if (!this.currency_prices) {return;}
 
-			currenry_prices = document.createElement('tbody');
+    console.log('RUB: ' + this.currency_prices.rates['RUB']);
+}
 
-			currenry_prices.forEach(function(data) {
+/*
+currency_prices_btn.onclick = function () {
 
-				let data_tr = document.createElement('tr'); // cteates <tr></tr>
+    currency_prices = document.createElement('tbody');
 
-				for (value in currenry_prices) {
-					let data_td = document.createElement('td'); // creates <td></td>
-					let text = document.createTextNode(data[value]); // creates text for the object value
-					data_td.appendChild(text); // adds the text to the <td>
-					data_tr.appendChild(data_td); // adds the <td> to the <tr>
-				}
+    currency_prices.forEach(function (data) {
 
-				currenry_prices.appendChild(data_tr); // adds <tr> to <tbody>
+        let data_tr = document.createElement('tr'); // cteates <tr></tr>
 
-			});
+        for (value in currency_prices) {
+            let data_td = document.createElement('td'); // creates <td></td>
+            let text = document.createTextNode(data[value]); // creates text for the object value
+            data_td.appendChild(text); // adds the text to the <td>
+            data_tr.appendChild(data_td); // adds the <td> to the <tr>
+        }
 
-			currenry_prices.appendChild(data_body); // pushes <tbody> to the DOM inside table
+        currency_prices.appendChild(data_tr); // adds <tr> to <tbody>
 
-	};
-	
+    });
 
+    currency_prices.appendChild(data_body); // pushes <tbody> to the DOM inside table
 
-
-
+};
+*/
